df8584c6abdd2f87f1984243ed91b3a7ee204a4e
3PILLAR\peter.drevicky <peter.drevicky@gmail.com>
2023-04-27 10:54:18 +0200

    add OnlineUsers component and show online users in it

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -5,7 +5,7 @@ import React from "react";
 
 export default function Avatar({ src }) {
   return (
-    <div>
+    <div className="avatar">
       <img src={src} alt="user avatar" />
     </div>
   );
