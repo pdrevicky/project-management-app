f0f5059800f5f061db65e02eed468257f5c1607f
3PILLAR\peter.drevicky <peter.drevicky@gmail.com>
2023-04-28 10:21:05 +0200

    add date fns library for comments

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -4,6 +4,7 @@ import { useAuthContext } from "../../hooks/useAuthContext";
 import uuid from "react-uuid";
 import { useFirestore } from "../../hooks/useFirestore";
 import Avatar from "../../components/Avatar";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 export default function ProjectComments({ project }) {
   const [newComment, setNewComment] = useState("");
@@ -43,7 +44,11 @@ export default function ProjectComments({ project }) {
                 <p>{comment.displayName}</p>
               </div>
               <div className="comment-date">
-                <p>date here</p>
+                <p>
+                  {formatDistanceToNow(comment.createdAt.toDate(), {
+                    addSuffix: true,
+                  })}
+                </p>
               </div>
               <div className="comment-content">
                 <p>{comment.content}</p>
